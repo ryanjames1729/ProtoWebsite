75a59306cc343a309c3a4485d4259bc27ce0450f
Dino <henry11223567@gmail.com>
2018-01-26 22:02:53 -0600

    Update App.py

--- App.py
+++ App.py
@@ -1,21 +1,19 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
-app = Flask(__name__)
+app = Flask(__name__,template_folder='C:/Users/admin/Documents/ProtoWebsite/ProtoWebsite/Template')
  
 @app.route("/", methods=['GET'])
 def index():
-    return "ProtoSite"
+    return render_template("test.html",)
  
 @app.route("/home", methods=['GET'])
 def home():
   name = "Henry"
-  return render_template(
-       'test.html',name=name)
+  return (name)
  
        
  
 if __name__ == "__main__":
     app.run()
-  
     
     #Run In command prompt "python (file-path)"
     #Open http://localhost:5000/
